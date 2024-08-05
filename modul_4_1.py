27a73845754cc6df0c4f8ca40d628cb09ca146a3
natashachelny <natasha-chelny70@mail.ru>
2024-08-05 12:03:42 +0300

    Add files via upload

---
+++ modul_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as ilona
+from true_math import divide as vadim
+
+result1 = ilona(69, 3)
+result2 = ilona(3, 0)
+result3 = vadim(49, 7)
+result4 = vadim(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
